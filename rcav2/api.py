d6888511fd6cb1d3bbeda2162da0c5f31f3db129
Tristan Cacqueray <tdecacqu@redhat.com>
2025-11-07 09:39:54 +0000

    Add field validation

--- rcav2/api.py
+++ rcav2/api.py
@@ -33,7 +33,7 @@ router = APIRouter()
 
 class RcaState:
     def __init__(self, debug):
-        self.env = Env(debug=True, cookie_path=None)
+        self.env = Env(debug=debug, cookie_path=None)
         rcav2.model.init_dspy()
 
 
